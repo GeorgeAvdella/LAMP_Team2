764ad7869fc09c90505e979b9c3e9a0a6c66d894
sidhys <sidhys1@gmail.com>
2026-01-29 14:15:07 -0500

    require name for add contact route

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -12,7 +12,7 @@ $last_name= trim($in_data["lastName"] ?? "");
 $phone = trim( $in_data["phone"] ?? "" );
 $email= trim($in_data["email"] ?? "");
 
-if ($user_id <= 0 || ($phone=="" && $email==""))
+if ($user_id <= 0 || ($phone=="" && $email=="") || ($first_name=="" || $last_name==""))
 {
 	header('Content-type: application/json');
 	echo json_encode(["error"=>"Missing required fields"]);
